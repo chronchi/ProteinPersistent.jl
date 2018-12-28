638ae971e297f6c75fa785dbb4bd10ee4d3fa511
Carlos <carloshvronchi@gmail.com>
2018-12-28 15:34:17 -0200

    update build

--- deps/build.jl
+++ deps/build.jl
@@ -6,19 +6,19 @@ import CondaBinDeps
 if lowercase(get(ENV, "CI", "false")) == "true"
 
     let basepython = get(ENV, "PYTHON", "python2")
-        envpath = joinpath(@__DIR__, "env")
-        run(`pip install --user virtualenv`)
-        run(`virtualenv --python=$basepython $envpath`)
+        #envpath = joinpath(@__DIR__, "env")
+        #run(`pip install --user virtualenv`)
+        #run(`virtualenv --python=$basepython $envpath`)
 
-        if Sys.iswindows()
-            python = joinpath(envpath, "Scripts", "python.exe")
-        else
-            python = joinpath(envpath, "bin", "python2")
-        end
+        #if Sys.iswindows()
+        #    python = joinpath(envpath, "Scripts", "python.exe")
+        #else
+        python = joinpath(@__DIR__, "bin", "python2")
+        #end
         run(`$python -m pip install Cython`)
         run(`$python -m pip install ripser`)
 
-        ENV["PYTHON"] = python
+        ENV["PYTHON"] = "$python"
         Pkg.build("PyCall")
 
         if VERSION >= v"0.7.0"
