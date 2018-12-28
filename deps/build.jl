a3e46d98cc6ed44323ce1e287fae16f404092087
Carlos <carloshvronchi@gmail.com>
2018-12-28 15:38:40 -0200

    update build

--- deps/build.jl
+++ deps/build.jl
@@ -6,13 +6,10 @@ import CondaBinDeps
 if lowercase(get(ENV, "CI", "false")) == "true"
 
     let basepython = get(ENV, "PYTHON", "python2")
-        #envpath = joinpath(@__DIR__, "env")
-        #run(`pip install --user virtualenv`)
-        #run(`virtualenv --python=$basepython $envpath`)
+        envpath = joinpath(@__DIR__, "env")
+        run(`pip install --user virtualenv`)
+        run(`virtualenv --python=$basepython $envpath`)
 
-        #if Sys.iswindows()
-        #    python = joinpath(envpath, "Scripts", "python.exe")
-        #else
         python = joinpath(@__DIR__, "bin", "python2")
         #end
         run(`$python -m pip install Cython`)
